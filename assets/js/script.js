070169b3e720f871c23043260a2f16b3bc578982
Chrystalio <chrystalio.k@outlook.com>
2023-12-30 17:24:55 +0700

    Add maxImages, selectedImageNumber also add function to getRandomNumber

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 const apiKey = "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";
 
+const maxImages = 4;
+let selectedImageNumber = null;
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
